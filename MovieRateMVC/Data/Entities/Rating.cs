9aab83c4d847d6dfff970f5f5a8c9108a194b98e
Bartłomiej Palus <bartlomiej.palus@poczta.onet.pl>
2025-11-11 23:09:04 +0100

    Add migration ChangeUserIdTypeInRatingTable

--- MovieRateMVC/Data/Entities/Rating.cs
+++ MovieRateMVC/Data/Entities/Rating.cs
@@ -14,7 +14,7 @@ namespace MovieRateMVC.Data.Entities
 		public Guid MovieId { get; set; }
 		public Movie Movie { get; set; }
 
-		public Guid? UserId { get; set; }
+		public string? UserId { get; set; }
 		public User? User { get; set; }
 	}
 }
---
+++ MovieRateMVC/Migrations/20251111220229_ChangeUserIdTypeInRatingTable.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRateMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserIdTypeInRatingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ratings_AspNetUsers_UserId1",
+                table: "Ratings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ratings_UserId1",
+                table: "Ratings");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Ratings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Ratings",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_UserId",
+                table: "Ratings",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ratings_AspNetUsers_UserId",
+                table: "Ratings",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ratings_AspNetUsers_UserId",
+                table: "Ratings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ratings_UserId",
+                table: "Ratings");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Ratings",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Ratings",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_UserId1",
+                table: "Ratings",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ratings_AspNetUsers_UserId1",
+                table: "Ratings",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
