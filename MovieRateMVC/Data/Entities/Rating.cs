021534958c9c3484ee82fd423e0946e59eb2926f
Bartłomiej Palus <bartlomiej.palus@poczta.onet.pl>
2025-11-11 22:13:33 +0100

    Add migration ChangeMarkRangeInRatingTable

--- MovieRateMVC/Data/Entities/Rating.cs
+++ MovieRateMVC/Data/Entities/Rating.cs
@@ -6,14 +6,15 @@ namespace MovieRateMVC.Data.Entities
 	{
 		public Guid Id { get; set; }
 
-		[Range(1, 10)]
+		[Range(1, 5)]
 		public int Mark { get; set; }
 		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+		[Required]
 		public Guid MovieId { get; set; }
 		public Movie Movie { get; set; }
 
-		public string? UserId { get; set; }
+		public Guid? UserId { get; set; }
 		public User? User { get; set; }
 	}
 }
